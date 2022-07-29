212ba015ad8fa4dfe1aeda64a001552a265a356a
Simon Schwertner <simonjulius.schwertner@student.hs-rm.de>
2022-07-29 14:02:16 +0200

    add particles

--- three-service/scene.js
+++ three-service/scene.js
@@ -6,8 +6,9 @@ import { FlyControls } from "three/examples/jsm/controls/FlyControls";
 
 import { Water } from "three/examples/jsm/objects/Water.js";
 import { Sky } from "three/examples/jsm/objects/Sky.js";
+import { ImprovedNoise } from "three/examples/jsm/math/ImprovedNoise.js";
 
-import { Boat, Trash } from "./swimming-parts.js";
+import { Boat, Trash, PlasticParticles } from "./swimming-parts.js";
 
 export class WaterScene {
   constructor() {
@@ -21,13 +22,14 @@ export class WaterScene {
     this.renderer = this.initRenderer();
     document.body.appendChild(this.renderer.domElement);
     this.scene = new THREE.Scene();
+    this.scene.fog = new THREE.FogExp2(0xffffff, 0.0001);
     this.camera = this.initCamera();
     this.controls = this.initControls();
 
     this.sun = new THREE.Vector3();
     this.sky = this.initSky();
     this.water = this.initWater();
-    this.bottom = this.initBottom();
+    this.terrain = this.initTerrain();
     this.pmremGenerator = new THREE.PMREMGenerator(this.renderer);
     this.updateSun();
 
@@ -35,14 +37,133 @@ export class WaterScene {
     this.trashes = [];
 
     this.initObjects();
+
+    this.plasticParticles = new PlasticParticles();
+    this.scene.add(this.plasticParticles.particles);
   }
 
-  initBottom() {
-    const bottom = this.loader.load("/assets/bottom/scene.gltf", (gltf) => {
-      gltf.scene.position.y = -10;
-      this.scene.add(gltf.scene);
-    });
-    return bottom;
+  initTerrain() {
+    const worldWidth = 256,
+      worldDepth = 256,
+      worldHalfWidth = worldWidth / 2,
+      worldHalfDepth = worldDepth / 2;
+    const data = this.generateHeight(worldWidth, worldDepth);
+
+    const geometry = new THREE.PlaneGeometry(
+      7500,
+      7500,
+      worldWidth - 1,
+      worldDepth - 1
+    );
+    geometry.rotateX(-Math.PI / 2);
+
+    const vertices = geometry.attributes.position.array;
+
+    for (let i = 0, j = 0, l = vertices.length; i < l; i++, j += 3) {
+      vertices[j + 1] = data[i] * 10;
+    }
+
+    //
+
+    const texture = new THREE.CanvasTexture(
+      this.generateTexture(data, worldWidth, worldDepth)
+    );
+    texture.wrapS = THREE.ClampToEdgeWrapping;
+    texture.wrapT = THREE.ClampToEdgeWrapping;
+
+    const mesh = new THREE.Mesh(
+      geometry,
+      new THREE.MeshBasicMaterial({ map: texture })
+    );
+    mesh.position.set(0, -1000, 0);
+    mesh.rotation.y = Math.PI / 2;
+    this.scene.add(mesh);
+    return mesh;
+  }
+
+  generateHeight(width, height) {
+    const size = width * height,
+      data = new Uint8Array(size),
+      perlin = new ImprovedNoise(),
+      z = Math.random() * 100;
+
+    let quality = 1;
+
+    for (let j = 0; j < 4; j++) {
+      for (let i = 0; i < size; i++) {
+        const x = i % width,
+          y = ~~(i / width);
+        data[i] += Math.abs(
+          perlin.noise(x / quality, y / quality, z) * quality * 1.75
+        );
+      }
+
+      quality *= 5;
+    }
+
+    return data;
+  }
+
+  generateTexture(data, width, height) {
+    // bake lighting into texture
+
+    let context, image, imageData, shade;
+
+    const vector3 = new THREE.Vector3(0, 0, 0);
+
+    const sun = new THREE.Vector3(1, 1, 1);
+    sun.normalize();
+
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+
+    context = canvas.getContext("2d");
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, width, height);
+
+    image = context.getImageData(0, 0, canvas.width, canvas.height);
+    imageData = image.data;
+
+    for (let i = 0, j = 0, l = imageData.length; i < l; i += 4, j++) {
+      vector3.x = data[j - 2] - data[j + 2];
+      vector3.y = 2;
+      vector3.z = data[j - width * 2] - data[j + width * 2];
+      vector3.normalize();
+
+      shade = vector3.dot(sun);
+
+      imageData[i] = (96 + shade * 128) * (0.5 + data[j] * 0.007);
+      imageData[i + 1] = (32 + shade * 96) * (0.5 + data[j] * 0.007);
+      imageData[i + 2] = shade * 96 * (0.5 + data[j] * 0.007);
+    }
+
+    context.putImageData(image, 0, 0);
+
+    // Scaled 4x
+
+    const canvasScaled = document.createElement("canvas");
+    canvasScaled.width = width * 4;
+    canvasScaled.height = height * 4;
+
+    context = canvasScaled.getContext("2d");
+    context.scale(4, 4);
+    context.drawImage(canvas, 0, 0);
+
+    image = context.getImageData(0, 0, canvasScaled.width, canvasScaled.height);
+    imageData = image.data;
+
+    for (let i = 0, l = imageData.length; i < l; i += 4) {
+      const v = ~~(Math.random() * 5);
+
+      imageData[i] += v;
+      imageData[i + 1] += v;
+      imageData[i + 2] += v;
+    }
+
+    context.putImageData(image, 0, 0);
+
+    return canvasScaled;
   }
 
   async initObjects() {
@@ -83,6 +204,14 @@ export class WaterScene {
 
     water.rotation.x = -Math.PI / 2;
     water2.rotation.x = Math.PI / 2;
+    water2.position.setY(-0.01);
+
+    // water.material.transparent = true;
+    // water.material.opacity = 0.1;
+    // water2.material.transparent = true;
+    // water2.material.opacity = 0.1;
+
+    console.log(water.material.opacity);
 
     this.scene.add(water);
     this.scene.add(water2);
@@ -125,8 +254,12 @@ export class WaterScene {
       sunDirection: new THREE.Vector3(),
       sunColor: 0xffffff,
       waterColor: 0x001e0f,
-      distortionScale: 3.7,
+      distortionScale: 0.1,
       fog: this.scene.fog !== undefined,
+      material: {
+        transparent: true,
+        opacity: 0.1,
+      },
     });
   }
 
@@ -191,6 +324,13 @@ export class WaterScene {
       this.boat.update();
       this.checkCollisions();
     }
+    if (this.trashes) {
+      this.trashes.forEach((trash) => {
+        if (trash.trash) {
+          trash.update();
+        }
+      });
+    }
     // stats.update();
   }
 
@@ -212,7 +352,18 @@ export class WaterScene {
       this.trashes.forEach((trash) => {
         if (trash.trash) {
           if (this.isColliding(this.boat.boat, trash.trash)) {
+            // console.log(
+            //   "Distance X:",
+            //   Math.abs(this.boat.boat.position.x - trash.trash.position.x)
+            // );
+            // console.log(
+            //   "Distance Y:",
+            //   Math.abs(this.boat.boat.position.y - trash.trash.position.y)
+            // );
             this.scene.remove(trash.trash);
+            this.trashes.filter((item) => {
+              return item !== trash;
+            });
           }
         }
       });
--- three-service/swimming-parts.js
+++ three-service/swimming-parts.js
@@ -1,38 +1,92 @@
-import { random } from './utils'
+import { random } from "./utils";
+import * as THREE from "three";
 
-export class Trash{
-    constructor(_scene){
-      _scene.scale.set(1.5, 1.5, 1.5)
-      _scene.position.set(random(-100, 100), -0.5, random(-100, 100))
-  
-      this.trash = _scene
-      
-    }
+export class Trash {
+  constructor(_scene) {
+    _scene.scale.set(1.5, 1.5, 1.5);
+    _scene.position.set(random(-100, 100), -0.5, random(-100, 100));
+
+    this.trash = _scene;
   }
 
+  update() {
+    let newX = this.trash.position.x - 0.01 + random(-0.01, 0.01);
+    let newY = -0.5 + random(-0.01, 0.01);
+    let newZ = this.trash.position.z - 0.01 + random(-0.01, 0.01);
+    this.trash.position.set(newX, newY, newZ);
+  }
+}
 
-export class Boat {
-    constructor(_scene) {
-        _scene.scale.set(3,3,3)
-        _scene.position.set(5,12.9,-20)
-        
-        this.boat = _scene
-        this.speed = {
-          vel: 0,
-          rot: 0
-        }
-    }
-  
-    update() {
-      if(this.boat) {
-        this.boat.rotation.y += this.speed.rot
-        this.boat.translateX(this.speed.vel)
+export class PlasticParticles {
+  constructor() {
+    this.SEPARATION = 100;
+    this.AMOUNTX = 50;
+    this.AMOUNTY = 50;
+
+    this.positions = new Float32Array(this.numParticles * 3);
+    this.scales = new Float32Array(this.numParticles);
+
+    let particles,
+      count = 0;
+
+    const numParticles = this.AMOUNTX * this.AMOUNTY;
+    let i = 0,
+      j = 0;
+    for (let ix = 0; ix < this.AMOUNTX; ix++) {
+      for (let iy = 0; iy < this.AMOUNTY; iy++) {
+        this.positions[i] =
+          ix * this.SEPARATION - (this.AMOUNTX * this.SEPARATION) / 2; // x
+        this.positions[i + 1] = 0; // y
+        this.positions[i + 2] =
+          iy * this.SEPARATION - (this.AMOUNTY * this.SEPARATION) / 2; // z
+
+        this.scales[j] = 1;
+
+        i += 3;
+        j++;
       }
     }
-  
-    stop() {
-      this.speed.rot = 0
-      this.speed.vel = 0
+
+    this.geometry = new THREE.BufferGeometry();
+    this.geometry.setAttribute(
+      "position",
+      new THREE.BufferAttribute(this.positions, 3)
+    );
+    this.geometry.setAttribute(
+      "scale",
+      new THREE.BufferAttribute(this.scales, 1)
+    );
+
+    this.material = new THREE.ShaderMaterial({
+      uniforms: {
+        color: { value: new THREE.Color(0xffffff) },
+      },
+    });
+    this.particles = new THREE.Points(this.geometry, this.material);
+  }
+}
+
+export class Boat {
+  constructor(_scene) {
+    _scene.scale.set(3, 3, 3);
+    _scene.position.set(5, 12.9, -20);
+
+    this.boat = _scene;
+    this.speed = {
+      vel: 0,
+      rot: 0,
+    };
+  }
+
+  update() {
+    if (this.boat) {
+      this.boat.rotation.y += this.speed.rot;
+      this.boat.translateX(this.speed.vel);
     }
   }
-  
+
+  stop() {
+    this.speed.rot = 0;
+    this.speed.vel = 0;
+  }
+}
